917e2379264d13a53434cf3a549e20b76890aa01
hansa241 <108556264+hansa241@users.noreply.github.com>
2025-01-27 15:53:25 +0100

    Added metadata to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css';
+import type {Metadata} from 'next';
 import React, {JSX} from "react";
+
+export const metadata: Metadata = {
+    title: 'The Garden Path'
+}
 
 export default function RootLayout({ children, }:
 { children: React.ReactNode }): JSX.Element {

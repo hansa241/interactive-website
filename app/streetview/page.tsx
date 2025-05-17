0295606ed8eb6d26331042d95909492830e6cc8b
hansa241 <108556264+hansa241@users.noreply.github.com>
2025-05-17 14:45:08 +0200

    Added Google Streetview page

---
+++ app/streetview/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React, {useCallback, useRef} from "react";
+import Script from "next/script";
+
+export default function Page() {
+    const panoRef = useRef<HTMLDivElement>(null);
+
+    const initialize = useCallback(() => {
+        if (panoRef.current === null) return;
+
+        new window.google.maps.StreetViewPanorama(panoRef.current, {
+            position: { lat: 44.947541243920625, lng: -93.10809415611486 },
+            pov: { heading: 0, pitch: 0},
+            zoom: 1,
+        });
+    }, []);
+
+    return (
+        <>
+            <Script
+                src={`https://maps.googleapis.com/maps/api/js?key=${process.env.STREETVIEW_API_KEY}`}
+                strategy={"afterInteractive"}
+                onLoad={initialize}
+            />
+            <main
+                id={"pano"}
+                ref={panoRef}
+                className="w-full h-screen"
+            ></main>
+        </>
+    )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    STREETVIEW_API_KEY: process.env.STREETVIEW_API_KEY,
+  },
 };
 
 export default nextConfig;

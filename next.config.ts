e493a622804793f6188d2e37a3f6d6f60466d15c
hansa241 <108556264+hansa241@users.noreply.github.com>
2025-05-29 12:00:27 +0200

    Adjusted config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     STREETVIEW_API_KEY: process.env.STREETVIEW_API_KEY,
+    STREETVIEW_USERNAME: process.env.STREETVIEW_USERNAME,
+    STREETVIEW_PASSWORD: process.env.STREETVIEW_PASSWORD,
   },
 };
 
